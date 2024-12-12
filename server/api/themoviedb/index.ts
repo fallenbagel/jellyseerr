2f08e092ae9e1baa0c713df336f7a1f7bda8fdb8
TOomaAh <tbrunet72@gmail.com>
2024-12-12 16:46:02 +0100

    fix(tmdb): fix build
    fix build after rebase

--- server/api/themoviedb/index.ts
+++ server/api/themoviedb/index.ts
@@ -348,6 +348,7 @@ class TheMovieDb extends ExternalAPI implements TvShowIndexer {
         }
         return episode;
       });
+
       return data;
     } catch (e) {
       throw new Error(`[TMDB] Failed to fetch TV show details: ${e.message}`);
