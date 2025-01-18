a085056c2aada98ca7c052b1b9012282136f896d
Pierre <63404022+0-Pierre@users.noreply.github.com>
2025-01-18 14:31:31 +0100

    fix: resolved issues with the music slider displaying all menus, and ensured media are properly removed from Lidarr.

--- src/components/ManageSlideOver/index.tsx
+++ src/components/ManageSlideOver/index.tsx
@@ -68,6 +68,7 @@ const messages = defineMessages('components.ManageSlideOver', {
   playedby: 'Played By',
   movie: 'movie',
   tvshow: 'series',
+  album: 'album',
 });
 
 const isMovie = (
@@ -485,9 +486,16 @@ const ManageSlideOver = ({
                             mediaType: intl.formatMessage(
                               mediaType === 'movie'
                                 ? messages.movie
+                                : mediaType === 'music'
+                                ? messages.album
                                 : messages.tvshow
                             ),
-                            arr: mediaType === 'movie' ? 'Radarr' : 'Sonarr',
+                            arr:
+                              mediaType === 'movie'
+                                ? 'Radarr'
+                                : mediaType === 'music'
+                                ? 'Lidarr'
+                                : 'Sonarr',
                           }
                         )}
                       </div>
@@ -712,7 +720,11 @@ const ManageSlideOver = ({
                   <div className="mt-2 text-xs text-gray-400">
                     {intl.formatMessage(messages.manageModalClearMediaWarning, {
                       mediaType: intl.formatMessage(
-                        mediaType === 'movie' ? messages.movie : messages.tvshow
+                        mediaType === 'movie'
+                          ? messages.movie
+                          : mediaType === 'music'
+                          ? messages.album
+                          : messages.tvshow
                       ),
                       mediaServerName:
                         settings.currentSettings.mediaServerType ===
