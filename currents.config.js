94e0b2ae51ad15c3b001a0b4bb173315f2d84c09
Fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2025-03-04 12:01:54 +0800

    chore(cypress): setup sorry cypress

---
+++ currents.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  projectId: 'jellyseerr',
+  recordKey: process.env.CYPRESS_RECORD_KEY,
+  cloudServiceUrl: process.env.SORRY_CYPRESS_DIRECTOR_URL || 'http://localhost:1234', 
+};
