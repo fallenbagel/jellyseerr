dcc74943da4d17e6251f5f75be639183dfa82f69
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2025-03-04 12:28:03 +0800

    style: apply prettier for real

--- currents.config.js
+++ currents.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   projectId: 'jellyseerr',
   recordKey: process.env.CYPRESS_RECORD_KEY,
-  cloudServiceUrl: process.env.SORRY_CYPRESS_DIRECTOR_URL || 'http://localhost:1234', 
+  cloudServiceUrl:
+    process.env.SORRY_CYPRESS_DIRECTOR_URL || 'http://localhost:1234',
 };
-
