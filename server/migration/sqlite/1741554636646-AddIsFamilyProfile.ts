8e55b9224006a6e67e7551e83936e867ea629cab
0xsysr3ll <31414959+0xSysR3ll@users.noreply.github.com>
2025-03-09 23:55:00 +0100

    fix: inconsistent class name

--- server/migration/sqlite/1741554636646-AddIsFamilyProfile.ts
+++ server/migration/sqlite/1741554636646-AddIsFamilyProfile.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export class AddIsFamilyProfile1610000000000 implements MigrationInterface {
+export class AddIsFamilyProfile1741554636646 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
       'user',
