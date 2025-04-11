mralien/overseerr

commit a488f850f3d6f0d9896362db873bd86ec1b7040a

refactor: switch from Fetch API to Axios (#1520)
* refactor: switch from Fetch API to Axios

* fix: remove unwanted changes

* fix: rewrite error handling for Axios and remove IPv4 first setting

* style: run prettier

* style: run prettier

* fix: add back custom proxy agent

* fix: add back custom proxy agent

* fix: correct rebase issue

* fix: resolve review comments

server/lib/imageproxy.ts
@@ -1,9 +1,8 @@
 import logger from '@server/logger';
-import type { RateLimitOptions } from '@server/utils/rateLimit';
-import rateLimit from '@server/utils/rateLimit';
+import axios from 'axios';
+import rateLimit, { type rateLimitOptions } from 'axios-rate-limit';
 import { createHash } from 'crypto';
 import { promises } from 'fs';
-import mime from 'mime/lite';
 import path, { join } from 'path';
 
 type ImageResponse = {
@@ -131,33 +130,29 @@ class ImageProxy {
     return 0;
   }
 
-  private fetch: typeof fetch;
+  private axios;
   private cacheVersion;
   private key;
-  private baseUrl;
-  private headers: HeadersInit | null = null;
 
   constructor(
     key: string,
     baseUrl: string,
     options: {
       cacheVersion?: number;
-      rateLimitOptions?: RateLimitOptions;
-      headers?: HeadersInit;
+      rateLimitOptions?: rateLimitOptions;
+      headers?: Record<string, string>;
     } = {}
   ) {
     this.cacheVersion = options.cacheVersion ?? 1;
-    this.baseUrl = baseUrl;
     this.key = key;
+    this.axios = axios.create({
+      baseURL: baseUrl,
+      headers: options.headers,
+    });
 
     if (options.rateLimitOptions) {
-      this.fetch = rateLimit(fetch, {
-        ...options.rateLimitOptions,
-      });
-    } else {
-      this.fetch = fetch;
+      this.axios = rateLimit(this.axios, options.rateLimitOptions);
     }
-    this.headers = options.headers || null;
   }
 
   public async getImage(
@@ -269,34 +264,19 @@ class ImageProxy {
   ): Promise<ImageResponse | null> {
     try {
       const directory = join(this.getCacheDirectory(), cacheKey);
-      const href =
-        this.baseUrl +
-        (this.baseUrl.length > 0
-          ? this.baseUrl.endsWith('/')
-            ? ''
-            : '/'
-          : '') +
-        (path.startsWith('/') ? path.slice(1) : path);
-      const response = await this.fetch(href, {
-        headers: this.headers || undefined,
+      const response = await this.axios.get(path, {
+        responseType: 'arraybuffer',
       });
-      if (!response.ok) {
-        return null;
-      }
-      const arrayBuffer = await response.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
 
-      const extension = mime.getExtension(
-        response.headers.get('content-type') ?? ''
-      );
-
+      const buffer = Buffer.from(response.data, 'binary');
+      const extension = path.split('.').pop() ?? '';
       let maxAge = Number(
-        (response.headers.get('cache-control') ?? '0').split('=')[1]
+        (response.headers['cache-control'] ?? '0').split('=')[1]
       );
 
       if (!maxAge) maxAge = 86400;
       const expireAt = Date.now() + maxAge * 1000;
-      const etag = (response.headers.get('etag') ?? '').replace(/"/g, '');
+      const etag = (response.headers.etag ?? '').replace(/"/g, '');
 
       await this.writeToCacheDir(
         directory,

server/routes/imageproxy.ts
@@ -5,6 +5,7 @@ import { Router } from 'express';
 const router = Router();
 const tmdbImageProxy = new ImageProxy('tmdb', 'https://image.tmdb.org', {
   rateLimitOptions: {
+    maxRequests: 20,
     maxRPS: 50,
   },
 });

fallenbagel/jellyseerr

commit d966bc7aaa123f7a39a559571188c83ca266516b

fix(imagecache): fix avatar cache folder creation
This fixes a regression caused by #1520 which broke the file extension detection thereby breaking
avatars

server/lib/imageproxy.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import rateLimit, { type rateLimitOptions } from 'axios-rate-limit';
 import { createHash } from 'crypto';
 import { promises } from 'fs';
+import mime from 'mime/lite';
 import path, { join } from 'path';
 
 type ImageResponse = {
@@ -269,7 +270,10 @@ class ImageProxy {
       });
 
       const buffer = Buffer.from(response.data, 'binary');
-      const extension = path.split('.').pop() ?? '';
+
+      const contentType = response.headers['content-type'] || '';
+      const extension = mime.getExtension(contentType) || '';
+
       let maxAge = Number(
         (response.headers['cache-control'] ?? '0').split('=')[1]
       );