5537e71d45bf13d456540ff83910cc45d1aa6f41
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2025-04-21 00:20:36 +0200

    feat(notifications): rename notification agent config type to agent

--- server/lib/settings/index.ts
+++ server/lib/settings/index.ts
@@ -189,7 +189,7 @@ export interface NotificationAgentConfig {
   types?: number;
   name: string;
   id?: number;
-  type: NotificationAgentKey;
+  agent: NotificationAgentKey;
   options: Record<string, unknown>;
 }
 
@@ -416,7 +416,7 @@ class Settings {
           email: {
             enabled: false,
             name: '',
-            type: NotificationAgentKey.EMAIL,
+            agent: NotificationAgentKey.EMAIL,
             options: {
               userEmailRequired: false,
               emailFrom: '',
@@ -433,7 +433,7 @@ class Settings {
             enabled: false,
             types: 0,
             name: '',
-            type: NotificationAgentKey.DISCORD,
+            agent: NotificationAgentKey.DISCORD,
             options: {
               webhookUrl: '',
               webhookRoleId: '',
@@ -444,7 +444,7 @@ class Settings {
             enabled: false,
             types: 0,
             name: '',
-            type: NotificationAgentKey.SLACK,
+            agent: NotificationAgentKey.SLACK,
             options: {
               webhookUrl: '',
             },
@@ -453,7 +453,7 @@ class Settings {
             enabled: false,
             types: 0,
             name: '',
-            type: NotificationAgentKey.TELEGRAM,
+            agent: NotificationAgentKey.TELEGRAM,
             options: {
               botAPI: '',
               chatId: '',
@@ -465,7 +465,7 @@ class Settings {
             enabled: false,
             types: 0,
             name: '',
-            type: NotificationAgentKey.PUSHBULLET,
+            agent: NotificationAgentKey.PUSHBULLET,
             options: {
               accessToken: '',
             },
@@ -474,7 +474,7 @@ class Settings {
             enabled: false,
             types: 0,
             name: '',
-            type: NotificationAgentKey.PUSHOVER,
+            agent: NotificationAgentKey.PUSHOVER,
             options: {
               accessToken: '',
               userToken: '',
@@ -485,7 +485,7 @@ class Settings {
             enabled: false,
             types: 0,
             name: '',
-            type: NotificationAgentKey.WEBHOOK,
+            agent: NotificationAgentKey.WEBHOOK,
             options: {
               webhookUrl: '',
               jsonPayload: '',
@@ -494,14 +494,14 @@ class Settings {
           webpush: {
             enabled: false,
             name: '',
-            type: NotificationAgentKey.WEBPUSH,
+            agent: NotificationAgentKey.WEBPUSH,
             options: {},
           },
           gotify: {
             enabled: false,
             types: 0,
             name: '',
-            type: NotificationAgentKey.GOTIFY,
+            agent: NotificationAgentKey.GOTIFY,
             options: {
               url: '',
               token: '',
