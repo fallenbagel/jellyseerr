b56e228f8682266a7b03bb61f128a3b962eeb037
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2025-05-07 16:14:19 +0200

    fix(notifications): repair ntfy notifications with rework

--- server/lib/settings/index.ts
+++ server/lib/settings/index.ts
@@ -531,6 +531,9 @@ class Settings {
           ntfy: {
             enabled: false,
             types: 0,
+            name: '',
+            id: 0,
+            agent: NotificationAgentKey.NTFY,
             options: {
               url: '',
               topic: '',
