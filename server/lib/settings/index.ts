ae8807bc1022a8d6660a0a1dfa8a9b0de215df93
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2025-05-22 00:08:30 +0200

    feat(notifications): set notification agent templates to be enabled

--- server/lib/settings/index.ts
+++ server/lib/settings/index.ts
@@ -92,7 +92,7 @@ class Settings {
         instances: [],
         agentTemplates: {
           email: {
-            enabled: false,
+            enabled: true,
             name: '',
             id: 0,
             agent: NotificationAgentKey.EMAIL,
@@ -109,7 +109,7 @@ class Settings {
             },
           },
           discord: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -121,7 +121,7 @@ class Settings {
             },
           },
           lunasea: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -131,7 +131,7 @@ class Settings {
             },
           },
           slack: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -141,7 +141,7 @@ class Settings {
             },
           },
           telegram: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -154,7 +154,7 @@ class Settings {
             },
           },
           pushbullet: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -164,7 +164,7 @@ class Settings {
             },
           },
           pushover: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -176,7 +176,7 @@ class Settings {
             },
           },
           webhook: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -187,14 +187,14 @@ class Settings {
             },
           },
           webpush: {
-            enabled: false,
+            enabled: true,
             name: '',
             id: 0,
             agent: NotificationAgentKey.WEBPUSH,
             options: {},
           },
           gotify: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
@@ -206,7 +206,7 @@ class Settings {
             },
           },
           ntfy: {
-            enabled: false,
+            enabled: true,
             types: 0,
             name: '',
             id: 0,
