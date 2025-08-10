e31281cac04f049e19ddb6ff218baf0c66f3bf70
gauthier-th <mail@gauthierth.fr>
2025-08-10 21:53:44 +0200

    fix: add env variable for min/max ttl & update dns-caching

--- server/utils/dnsCache.ts
+++ server/utils/dnsCache.ts
@@ -1,5 +1,9 @@
 import logger from '@server/logger';
 import { DnsCacheManager } from 'dns-caching';
 
-const dnsCache = new DnsCacheManager({ logger });
+const dnsCache = new DnsCacheManager({
+  logger: logger,
+  forceMaxTtl: Number(process.env.DNS_CACHE_FORCE_MAX_TTL) || -1,
+  forceMinTtl: Number(process.env.DNS_CACHE_FORCE_MIN_TTL) || 0,
+});
 export default dnsCache;
